263187e82263fc45acce80512d3d7015f1861e57
abhi <abhishekdudhal@gmail.com>
2018-01-27 15:16:12 +0530

    CNN code #coded by abhishekdudhal

--- CNN/cnnTrain.py
+++ CNN/cnnTrain.py
@@ -12,7 +12,6 @@ from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split
 import os
 from keras.utils import np_utils
-import matplotlib
 from matplotlib import pyplot as plt
 
 backend.set_image_dim_ordering('th')
